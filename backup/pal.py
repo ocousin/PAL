cd37aaff505f1c1df177accd41a2883c9c55fe32
Olivier Cousin <olivier.cousin@gmail.com>
2014-09-02 20:33:53 +0100

    I am back!

---
+++ backup/pal.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+import sys, getopt
+import xml.etree.ElementTree as ET
+import quartusFiles
+
+
+
+print "Welcome to PAL, let me help you to build you reference design"
+def main(argv):
+    projectName = ''
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hrn:i:o:",["prjfile=","ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'pal.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'This is your help menu pal!'
+            print 'pal.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+            sys.exit()
+        elif opt in ("-n", "--prjfile"):
+            projectName = arg
+            print'Your project will be called',projectName
+            #fqpf = open(projectName + 'qpf', 'w')
+            #fqsf = open(projectName +'.qsf', 'w')
+            quartusFiles.createQSF(projectName)
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+        elif opt == '-r':
+            print 'opening the input file'
+            f = open(inputfile, 'r')
+            for line in f:
+                print line,
+
+    print 'Input file is "', inputfile
+    print 'Output file is "', outputfile
+    
+    #Let's open the xml declaration to see what's in it
+    tree = ET.parse(inputfile)
+    root = tree.getroot()
+    
+    
+    
+    #f = open(inputfile, 'r')
+    #for line in f:
+    #    print line,
+        
+    #fqpf = open('test.qpf', 'w')
+    #fqpf.write('This is a test for the qpf file creation\n')
+#  
+#  fqsf = open(outputfile.qsf, 'w')
+    #fqsf.write('This is a test for the qsf file creation\n')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
