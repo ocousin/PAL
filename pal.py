d31cc7341c7d8d5bcb0da8747b583f33bcd141b2
ocousin <ocousin@uk-LaGaillette.altera.com>
2014-09-03 08:39:33 +0100

    try with eclipse

--- pal.py
+++ pal.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 
 #this is a test to understand git -test commit from work
 
+#this is a test to understand git -test commit from work and fron eclipse
 
 #this is a test to check the push function from work
 
