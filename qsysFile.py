6c4d4dbb288f23324714373864b9312e1ba16d0c
ocousin <olivier.cousin@gmail.com>
2014-10-10 13:39:17 +0100

    adding the tcl script

--- qsysFile.py
+++ qsysFile.py
@@ -8,7 +8,7 @@ import xml.etree.ElementTree as ET
 #qsysScript = '/build/localdata/altera/Linux/quartusII-14.0/quartus/sopc_builder/bin'
 #os.environ['PATH']=  os.environ['PATH']+ qsysScript
 #os.system('echo $PATH')
-
+print 'Start\n'
 os.environ["QSYS"] = "/build/localdata/altera/Linux/quartusII-14.0/quartus/sopc_builder/bin"
 os.getenv("QSYS")
 os.environ["PATH"] += os.pathsep + os.getenv("QSYS")
@@ -18,6 +18,8 @@ os.environ["PATH"] += os.pathsep + os.getenv("QSYS")
 #os.system('export PATH=$PATH:/build/localdata/altera/Linux/quartusII-14.0/quartus/sopc_builder/bin')
 #os.system('export PATH=$PATH:totototototottototottot')
 os.system('echo $PATH')
-qsysCmd='qsys-script --help'
+print 'Running qsys-script\n'
+
+qsysCmd='qsys-script --script ./test/qsys.tcl'
 os.system(qsysCmd)
 
